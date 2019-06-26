51db8b2ee8dd3ff779a682b21f77bdc6c26d4c2f
JokerJun <tuwe_njun@126.com>
2019-06-26 14:53:00 +0800

    提交一次maven工程

---
+++ GitStudy/src/main/java/cn/jokerjun/entity/User.java
@@ -0,0 +1,31 @@
+package cn.jokerjun.entity;
+
+public class User {
+    private int age;
+    private String name;
+    private String password;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
